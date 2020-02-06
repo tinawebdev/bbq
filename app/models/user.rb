2942c1a4e223116371a0da34289c9f2f734ffab4
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-02-06 16:05:46 +0300

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Пользователь №#{rand(777)}" if self.name.blank?
+  end
+end
