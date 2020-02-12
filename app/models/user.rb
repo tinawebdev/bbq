774a76be7db6eecc9180593e84406c43b9146000
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-02-12 15:17:15 +0300

    added ability to update subscription if user is registered

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,14 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: 35}
   before_validation :set_name, on: :create
+  after_commit :link_subscriptions, on: :create
 
   private
+  
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
+  end
 
   def set_name
     self.name = "Пользователь №#{rand(777)}" if self.name.blank?
