1bdbc47fe1f39618fbc572fd915b3b6fa7502ca0
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-03-13 14:23:56 +0300

    carrierwave added

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'devise'
 gem 'devise-i18n'
 gem 'rails-i18n', '~> 6.0.0'
+gem 'carrierwave'
+gem 'rmagick'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,41 @@
 module ApplicationHelper
   def user_avatar(user)
-    asset_path('user.png')
+    if user.avatar?
+      user.avatar.url
+    else
+      asset_path('user.png')
+    end
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('user.png')
+    end
   end
 
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fas fa-#{icon_class}"
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
   end
 end
