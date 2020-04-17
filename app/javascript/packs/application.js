77ee744d8356f03d0f37e7c338735851f6b5ee70
Kristina <codingkris@yahoo.com>
2020-04-17 14:26:45 +0300

    Added yandex map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 import '../stylesheets/application'
 import './bootstrap_custom.js'
 import 'components/lightbox/ekko-lightbox.min.js'
+import 'components/map.js'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
