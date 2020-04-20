f1a7ec0286744864708cce9f56a5e70d14f56987
Kristina <codingkris@yahoo.com>
2020-04-20 16:12:16 +0300

    Datetime validate added

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,10 +9,19 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
+  validate :can_not_in_the_past
 
   scope :sorted, -> { order(datetime: :desc) }
 
   def visitors
     (subscribers + [user]).uniq
+  end
+
+  private
+
+  def can_not_in_the_past
+    if datetime < Time.now
+      errors.add(:datetime, I18n.t('event.can_not_in_the_past'))
+    end
   end
 end
